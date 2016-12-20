1b03f8f7035c5563ae447eb78c61d758d0f144c0
SapereAude86 <sdehaan1986@gmail.com>
2016-12-20 08:47:21 +0100

    iets verder uitgewerkt, moet nog wel iets aan lose urls gedaan worden

--- webcr.py
+++ webcr.py
@@ -1,10 +1,31 @@
 from lxml import html
 import requests
+import os
+import getpass
+import subprocess
+
 
 def grab_pages(websites):
 	page = requests.get(websites)
 	tree = html.fromstring(page.content)
 	images = tree.xpath('//img/@src')
 	return images
-for image in grab_pages('http://nu.nl'):
-	print image
+
+website = raw_input("geef website: ")
+username = getpass.getuser()
+image_dir = '/Users/' + username + '/Desktop/crawler_images/'
+if not os.path.isdir(image_dir):
+	print "Creating directory..."
+	os.mkdir(image_dir)
+
+for image in grab_pages(website):
+	commando = "curl -o " + image_dir + image.split("/")[-1] + " " + image
+	if "http" in commando or "www" in commando:
+		p1 = subprocess.Popen(commando, shell=True)
+		p1.communicate()
+	else:
+		print "Geef goede url..."
+		print commando
+
+
+	
